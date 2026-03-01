311cc539f388ee31d0e41a47ee08bccb38c26282
Masaki Kagaya <masakielastic@gmail.com>
2026-03-01 21:09:47 +0900

    Add client UDP socket support

--- ext/quic_client.c
+++ ext/quic_client.c
@@ -5,12 +5,283 @@
 #include "quic_client.h"
 #include "quic_stream.h"
 
+#include "main/php_network.h"
+#include "main/php_streams.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+typedef struct _quic_client_connection_object {
+  php_socket_t fd;
+  zend_string *host;
+  zend_string *port;
+  struct sockaddr_storage local_addr;
+  socklen_t local_addrlen;
+  struct sockaddr_storage peer_addr;
+  socklen_t peer_addrlen;
+  zend_object std;
+} quic_client_connection_object;
+
 zend_class_entry *quic_client_connection_ce;
+
+static zend_object_handlers quic_client_connection_handlers;
+
+static inline quic_client_connection_object *quic_client_connection_from_obj(zend_object *object)
+{
+  return (quic_client_connection_object *) ((char *) object - XtOffsetOf(quic_client_connection_object, std));
+}
+
+#define Z_QUIC_CLIENT_CONNECTION_P(zv) \
+  quic_client_connection_from_obj(Z_OBJ_P((zv)))
+
+static zend_string *quic_client_port_to_string(zval *port)
+{
+  char buffer[32];
+  int length;
+
+  if (Z_TYPE_P(port) == IS_LONG) {
+    if (Z_LVAL_P(port) <= 0 || Z_LVAL_P(port) > 65535) {
+      zend_argument_value_error(2, "must be between 1 and 65535");
+      return NULL;
+    }
+
+    length = snprintf(buffer, sizeof(buffer), "%ld", Z_LVAL_P(port));
+    if (length <= 0) {
+      zend_throw_exception_ex(quic_exception_ce, 0, "Failed to format port");
+      return NULL;
+    }
+
+    return zend_string_init(buffer, (size_t) length, 0);
+  }
+
+  if (Z_TYPE_P(port) == IS_STRING) {
+    if (Z_STRLEN_P(port) == 0) {
+      zend_argument_must_not_be_empty_error(2);
+      return NULL;
+    }
+
+    return zend_string_copy(Z_STR_P(port));
+  }
+
+  zend_argument_type_error(2, "must be of type string|int");
+  return NULL;
+}
+
+static bool quic_client_set_nonblocking(php_socket_t fd)
+{
+  int flags = fcntl(fd, F_GETFL, 0);
+
+  if (flags < 0) {
+    zend_throw_exception_ex(
+      quic_exception_ce,
+      errno,
+      "fcntl(F_GETFL) failed: %s",
+      strerror(errno)
+    );
+    return false;
+  }
+
+  if ((flags & O_NONBLOCK) == 0 && fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0) {
+    zend_throw_exception_ex(
+      quic_exception_ce,
+      errno,
+      "fcntl(F_SETFL) failed: %s",
+      strerror(errno)
+    );
+    return false;
+  }
+
+  return true;
+}
+
+static bool quic_client_connect_socket(
+  quic_client_connection_object *intern,
+  zend_string *host,
+  zend_string *port
+)
+{
+  struct addrinfo hints = {0};
+  struct addrinfo *result = NULL;
+  struct addrinfo *rp;
+  int rv;
+
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_DGRAM;
+
+  rv = getaddrinfo(ZSTR_VAL(host), ZSTR_VAL(port), &hints, &result);
+  if (rv != 0) {
+    zend_throw_exception_ex(
+      quic_exception_ce,
+      0,
+      "getaddrinfo(%s, %s) failed: %s",
+      ZSTR_VAL(host),
+      ZSTR_VAL(port),
+      gai_strerror(rv)
+    );
+    return false;
+  }
+
+  for (rp = result; rp != NULL; rp = rp->ai_next) {
+    php_socket_t fd;
+
+    fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+    if (fd < 0) {
+      continue;
+    }
+
+    if (connect(fd, rp->ai_addr, rp->ai_addrlen) == 0) {
+      socklen_t local_addrlen = sizeof(intern->local_addr);
+
+      if (!quic_client_set_nonblocking(fd)) {
+        close(fd);
+        freeaddrinfo(result);
+        return false;
+      }
+
+      if (getsockname(fd, (struct sockaddr *) &intern->local_addr, &local_addrlen) != 0) {
+        zend_throw_exception_ex(
+          quic_exception_ce,
+          errno,
+          "getsockname() failed: %s",
+          strerror(errno)
+        );
+        close(fd);
+        freeaddrinfo(result);
+        return false;
+      }
+
+      intern->fd = fd;
+      intern->local_addrlen = local_addrlen;
+      intern->peer_addrlen = rp->ai_addrlen;
+      memcpy(&intern->peer_addr, rp->ai_addr, rp->ai_addrlen);
+
+      freeaddrinfo(result);
+      return true;
+    }
+
+    close(fd);
+  }
+
+  freeaddrinfo(result);
+
+  zend_throw_exception_ex(
+    quic_exception_ce,
+    errno,
+    "Could not connect UDP socket to %s:%s: %s",
+    ZSTR_VAL(host),
+    ZSTR_VAL(port),
+    strerror(errno)
+  );
+
+  return false;
+}
+
+static void quic_client_address_to_array(
+  zval *return_value,
+  const struct sockaddr *addr,
+  socklen_t addrlen
+)
+{
+  char host[NI_MAXHOST];
+  char service[NI_MAXSERV];
+  long port;
+  int rv;
+
+  array_init(return_value);
+
+  if (addr == NULL || addrlen == 0) {
+    return;
+  }
+
+  rv = getnameinfo(
+    addr,
+    addrlen,
+    host,
+    sizeof(host),
+    service,
+    sizeof(service),
+    NI_NUMERICHOST | NI_NUMERICSERV
+  );
+
+  if (rv != 0) {
+    add_assoc_null(return_value, "family");
+    add_assoc_null(return_value, "address");
+    add_assoc_null(return_value, "port");
+    return;
+  }
+
+  switch (addr->sa_family) {
+    case AF_INET:
+      add_assoc_string(return_value, "family", "AF_INET");
+      break;
+    case AF_INET6:
+      add_assoc_string(return_value, "family", "AF_INET6");
+      break;
+    default:
+      add_assoc_string(return_value, "family", "AF_UNSPEC");
+      break;
+  }
+
+  add_assoc_string(return_value, "address", host);
+
+  port = strtol(service, NULL, 10);
+  add_assoc_long(return_value, "port", port);
+}
+
+static void quic_client_connection_free_object(zend_object *object)
+{
+  quic_client_connection_object *intern = quic_client_connection_from_obj(object);
+
+  if (intern->fd >= 0) {
+    close(intern->fd);
+    intern->fd = -1;
+  }
+
+  if (intern->host != NULL) {
+    zend_string_release(intern->host);
+    intern->host = NULL;
+  }
+
+  if (intern->port != NULL) {
+    zend_string_release(intern->port);
+    intern->port = NULL;
+  }
+
+  zend_object_std_dtor(&intern->std);
+}
+
+static zend_object *quic_client_connection_create_object(zend_class_entry *class_type)
+{
+  quic_client_connection_object *intern;
+
+  intern = zend_object_alloc(sizeof(quic_client_connection_object), class_type);
+  intern->fd = -1;
+  intern->host = NULL;
+  intern->port = NULL;
+  intern->local_addrlen = 0;
+  intern->peer_addrlen = 0;
+
+  zend_object_std_init(&intern->std, class_type);
+  object_properties_init(&intern->std, class_type);
+  intern->std.handlers = &quic_client_connection_handlers;
+
+  return &intern->std;
+}
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_quic_client_construct, 0, 0, 2)
   ZEND_ARG_TYPE_INFO(0, host, IS_STRING, 0)
   ZEND_ARG_TYPE_MASK(0, port, MAY_BE_LONG | MAY_BE_STRING, NULL)
   ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(0, options, IS_ARRAY, 0, "[]")
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_quic_client_get_stream, 0, 0, IS_RESOURCE, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_quic_client_void, 0, 0, 0)
@@ -35,37 +306,72 @@ ZEND_END_ARG_INFO()
 
 PHP_METHOD(Quic_ClientConnection, __construct)
 {
+  quic_client_connection_object *intern = Z_QUIC_CLIENT_CONNECTION_P(ZEND_THIS);
   zend_string *host;
-  zval *port;
+  zend_string *port = NULL;
+  zval *port_value;
   zval *options = NULL;
 
   ZEND_PARSE_PARAMETERS_START(2, 3)
     Z_PARAM_STR(host)
-    Z_PARAM_ZVAL(port)
+    Z_PARAM_ZVAL(port_value)
     Z_PARAM_OPTIONAL
     Z_PARAM_ARRAY(options)
   ZEND_PARSE_PARAMETERS_END();
+
+  if (intern->fd >= 0) {
+    zend_throw_error(NULL, "Cannot reinitialize Quic\\ClientConnection");
+    RETURN_THROWS();
+  }
 
   if (ZSTR_LEN(host) == 0) {
     zend_argument_must_not_be_empty_error(1);
     RETURN_THROWS();
   }
 
-  if (Z_TYPE_P(port) == IS_LONG && Z_LVAL_P(port) <= 0) {
-    zend_argument_value_error(2, "must be greater than 0");
+  port = quic_client_port_to_string(port_value);
+  if (port == NULL) {
     RETURN_THROWS();
   }
 
-  if (Z_TYPE_P(port) == IS_STRING && Z_STRLEN_P(port) == 0) {
-    zend_argument_must_not_be_empty_error(2);
+  intern->host = zend_string_copy(host);
+  intern->port = port;
+
+  if (!quic_client_connect_socket(intern, host, port)) {
     RETURN_THROWS();
   }
 }
 
 PHP_METHOD(Quic_ClientConnection, getStream)
 {
-  quic_throw_not_implemented("Quic\\ClientConnection::getStream");
-  RETURN_THROWS();
+  quic_client_connection_object *intern = Z_QUIC_CLIENT_CONNECTION_P(ZEND_THIS);
+  php_stream *stream;
+  php_socket_t dup_fd;
+
+  if (intern->fd < 0) {
+    zend_throw_exception_ex(quic_exception_ce, 0, "Connection is closed");
+    RETURN_THROWS();
+  }
+
+  dup_fd = dup(intern->fd);
+  if (dup_fd < 0) {
+    zend_throw_exception_ex(
+      quic_exception_ce,
+      errno,
+      "dup() failed: %s",
+      strerror(errno)
+    );
+    RETURN_THROWS();
+  }
+
+  stream = php_stream_sock_open_from_socket(dup_fd, NULL);
+  if (stream == NULL) {
+    close(dup_fd);
+    zend_throw_exception_ex(quic_exception_ce, 0, "Failed to expose UDP socket as a PHP stream");
+    RETURN_THROWS();
+  }
+
+  php_stream_to_zval(stream, return_value);
 }
 
 PHP_METHOD(Quic_ClientConnection, startHandshake)
@@ -110,6 +416,7 @@ PHP_METHOD(Quic_ClientConnection, openBidirectionalStream)
 
 PHP_METHOD(Quic_ClientConnection, close)
 {
+  quic_client_connection_object *intern = Z_QUIC_CLIENT_CONNECTION_P(ZEND_THIS);
   zval *error_code = NULL;
   zend_string *reason = NULL;
 
@@ -126,23 +433,37 @@ PHP_METHOD(Quic_ClientConnection, close)
 
   (void) reason;
 
-  quic_throw_not_implemented("Quic\\ClientConnection::close");
-  RETURN_THROWS();
+  if (intern->fd >= 0) {
+    close(intern->fd);
+    intern->fd = -1;
+  }
 }
 
 PHP_METHOD(Quic_ClientConnection, getPeerAddress)
 {
-  array_init(return_value);
+  quic_client_connection_object *intern = Z_QUIC_CLIENT_CONNECTION_P(ZEND_THIS);
+
+  quic_client_address_to_array(
+    return_value,
+    (const struct sockaddr *) &intern->peer_addr,
+    intern->peer_addrlen
+  );
 }
 
 PHP_METHOD(Quic_ClientConnection, getLocalAddress)
 {
-  array_init(return_value);
+  quic_client_connection_object *intern = Z_QUIC_CLIENT_CONNECTION_P(ZEND_THIS);
+
+  quic_client_address_to_array(
+    return_value,
+    (const struct sockaddr *) &intern->local_addr,
+    intern->local_addrlen
+  );
 }
 
 static const zend_function_entry quic_client_connection_methods[] = {
   PHP_ME(Quic_ClientConnection, __construct, arginfo_quic_client_construct, ZEND_ACC_PUBLIC)
-  PHP_ME(Quic_ClientConnection, getStream, arginfo_quic_client_void, ZEND_ACC_PUBLIC)
+  PHP_ME(Quic_ClientConnection, getStream, arginfo_quic_client_get_stream, ZEND_ACC_PUBLIC)
   PHP_ME(Quic_ClientConnection, startHandshake, arginfo_quic_client_void, ZEND_ACC_PUBLIC)
   PHP_ME(Quic_ClientConnection, handleReadable, arginfo_quic_client_void, ZEND_ACC_PUBLIC)
   PHP_ME(Quic_ClientConnection, handleExpiry, arginfo_quic_client_void, ZEND_ACC_PUBLIC)
@@ -162,4 +483,10 @@ void quic_client_register_classes(void)
 
   INIT_NS_CLASS_ENTRY(ce, "Quic", "ClientConnection", quic_client_connection_methods);
   quic_client_connection_ce = zend_register_internal_class(&ce);
+  quic_client_connection_ce->create_object = quic_client_connection_create_object;
+
+  memcpy(&quic_client_connection_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers));
+  quic_client_connection_handlers.offset = XtOffsetOf(quic_client_connection_object, std);
+  quic_client_connection_handlers.free_obj = quic_client_connection_free_object;
+  quic_client_connection_handlers.clone_obj = NULL;
 }
---
+++ ext/tests/002_client_socket.phpt
@@ -0,0 +1,44 @@
+--TEST--
+Quic\ClientConnection creates a UDP socket stream and exposes addresses
+--SKIPIF--
+<?php
+if (!extension_loaded('quic')) {
+    echo 'skip extension not loaded';
+}
+?>
+--FILE--
+<?php
+$client = new Quic\ClientConnection('127.0.0.1', 4433);
+$stream = $client->getStream();
+$peer = $client->getPeerAddress();
+$local = $client->getLocalAddress();
+
+var_dump(is_resource($stream));
+var_dump(get_resource_type($stream));
+var_dump($peer['family']);
+var_dump($peer['address']);
+var_dump($peer['port']);
+var_dump($local['family']);
+var_dump($local['address']);
+var_dump(is_int($local['port']));
+var_dump($local['port'] > 0);
+
+$client->close();
+
+try {
+    $client->getStream();
+} catch (Quic\Exception $e) {
+    echo $e->getMessage(), PHP_EOL;
+}
+?>
+--EXPECTF--
+bool(true)
+string(%d) "stream"
+string(7) "AF_INET"
+string(9) "127.0.0.1"
+int(4433)
+string(7) "AF_INET"
+string(9) "127.0.0.1"
+bool(true)
+bool(true)
+Connection is closed
